ed6b007f534aefe82daf708a9e98397ba411a0af
Samudrala Venkata Pavan Tarun Kumar <116933974+klu-2200031955@users.noreply.github.com>
2024-04-29 17:41:29 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
-    // url: 'https://backendapp-jr9d.onrender.com'
-    url: 'http://localhost:2032'
+    url: 'https://backendapp-jr9d.onrender.com'
+    // url: 'http://localhost:2032'
 };
 
 export default config;
